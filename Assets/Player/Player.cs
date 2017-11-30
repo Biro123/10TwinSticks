c7a549cca5e07ab2201eff65e8f7b10a0d69d01b
b <biro123@ntlworld.com>
2017-11-30 15:22:58 +0000

    Imported Player Characters assets, used Roller ball. Re-organised folders and added .GitIgnore.

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( CrossPlatformInputManager.GetAxis("Horizontal") < 0f)
+        {            
+            Debug.Log("Left Pressed");
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0f)
+        {
+            Debug.Log("Right Pressed");
+        }
+    }
+}
